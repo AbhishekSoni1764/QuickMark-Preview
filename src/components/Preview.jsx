f883957ecf109cd27896b636470d7b853ddbb3ba
Abhishek <abhisoni1764@gmail.com>
2024-03-28 01:53:02 +0530

    Preview Background Updated

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -9,8 +9,8 @@ export default function Preview({ markdown, isPreview, isVert }) {
     return (
         <>
             {isPreview && (
-                <div className={isVert ? "w-[-webkit-fill-available] h-1/2 p-5 m-5 border-2 border-grey-200 rounded-md bg-grey-300" : "w-[-webkit-fill-available] lg:w-1/2 h-fit lg:h-screen p-5 m-5 border-2 border-grey-200 rounded-md bg-grey-300"}>
-                    <div className="w-full max-w-full h-full overflow-y-scroll prose tracking-wider bg-grey-300 prose-invert prose-a:text-blue-600" dangerouslySetInnerHTML={{ __html: text }} />
+                <div className={isVert ? "w-[-webkit-fill-available] h-1/2 p-5 m-5 border-2 border-grey-200 rounded-md bg-grey-250" : "w-[-webkit-fill-available] lg:w-1/2 h-fit lg:h-screen p-5 m-5 border-2 border-grey-200 rounded-md bg-grey-250"}>
+                    <div className="w-full max-w-full h-full overflow-y-scroll prose tracking-wider bg-grey-250 prose-invert prose-a:text-blue-600" dangerouslySetInnerHTML={{ __html: text }} />
                 </div>
             )}
         </>
