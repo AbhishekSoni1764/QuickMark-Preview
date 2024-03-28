3e826ab812de59771e5310be17b1d8ce6d42950b
Abhishek <abhisoni1764@gmail.com>
2024-03-28 09:28:01 +0530

    View Changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     const showPreview = (e) => {
-      if (e.shiftKey && e.key === "Enter") {
+      if (e.shiftKey || e.key === "Enter") {
         e.preventDefault();
         setIsPreview(prevValue => !prevValue);
       }
